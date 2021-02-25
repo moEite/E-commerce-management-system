b8b0af41afd925d3ee8beedc0c51766aab7b752b
moEdit <1324255656@qq.com>
2021-02-25 10:26:11 +0800

    '0.0'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.use(ElementUi)
 
 new Vue({
+  axios,
   router,
   render: h => h(App)
 }).$mount('#app')
